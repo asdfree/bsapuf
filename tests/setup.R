5aa691859b154bdb6b4f2c1e5f199990f0345640
Anthony Damico <ajdamico@gmail.com>
2018-01-09 14:52:57 -0500

    bsapuf

--- tests/setup.R
+++ tests/setup.R
@@ -1,7 +1,12 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
 library(lodown)
-lodown( "bsapuf" , output_dir = file.path( getwd() ) )
+this_sample_break <- Sys.getenv( "this_sample_break" )
+bsapuf_cat <- get_catalog( "bsapuf" , output_dir = file.path( getwd() ) )
+record_categories <- ceiling( seq( nrow( bsapuf_cat ) ) / ceiling( nrow( bsapuf_cat ) / 32 ) )
+bsapuf_cat <- bsapuf_cat[ record_categories == this_sample_break , ]
+lodown( "bsapuf" , bsapuf_cat )
+if( any( bsapuf_cat$db_tablename == 'bsa_partd_events_2008' ) ){
 library(DBI)
 dbdir <- file.path( getwd() , "SQLite.db" )
 db <- dbConnect( RSQLite::SQLite() , dbdir )
@@ -116,3 +121,4 @@ bsapuf_tbl %>%
 	group_by( bene_sex_ident_cd ) %>%
 	summarize( mean = mean( pde_drug_cost ) )
 dbGetQuery( db , "SELECT COUNT(*) FROM bsa_partd_events_2008" )
+}
